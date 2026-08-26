1c987ecb145ab41f21bdff43e7a079f0204754a8
Prakriti Shakya <prakritishakya1168@gmail.com>
2026-08-25 19:58:37 -0700

    silver model modified orders

--- walmart_project/models/silver_t/orders_t.sql
+++ walmart_project/models/silver_t/orders_t.sql
@@ -13,7 +13,4 @@ where is_active = 'Y'
 
 {% if is_incremental() %}
   and updated_timestamp > (select COALESCE(max(updated_timestamp, '1900-01-01'), '1900-01-01') from {{ this }})
-{% endif %};
-
-select *, current_timestamp() as processed_at
- from {{source('walmart_databricks', 'products')}};
+{% endif %}
